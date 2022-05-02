64c985d4ece6908dddbb9f720d2391d406c8d8f6
István Siroki <stevesiroki@gmail.com>
2022-05-02 21:14:12 +0100

    Use 12 threads in sync_all_I_want and make it runable from anywhere

--- sync_all_I_want.ps1
+++ sync_all_I_want.ps1
@@ -1,3 +1,3 @@
-. .\utils.ps1
-.\sync_odrive.ps1 -ExcludePattern "Shared With Me|Steve-OneDrive\\Videos\\|Steve-OneDrive\\Pictures\\|Amazon Cloud Drive" -FolderExpandExcludePattern "Shared With Me" -JobCount 16
+. $PSScriptRoot\utils.ps1
+& $PSScriptRoot\sync_odrive.ps1 -ExcludePattern "Shared With Me|Steve-OneDrive\\Videos\\|Steve-OneDrive\\Pictures\\|Amazon Cloud Drive" -FolderExpandExcludePattern "Shared With Me" -JobCount 12
 pause "Press any key to continue"
